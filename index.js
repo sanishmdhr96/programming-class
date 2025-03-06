e1e352532050f254db92c4087234d60a609aa27d
sanishmdhr96 <mail.sanishmanandhar@gmail.com>
2025-03-06 12:25:29 +0545

    for loop class 1

---
+++ index.js
@@ -0,0 +1,85 @@
+// function add(a, b) {
+//     return a + b
+// }
+
+// function subtract(a, b) {
+//     return a - b
+// }
+
+
+// function multiply(a, b) {
+//     return a * b
+// }
+
+
+// function divide(a, b) {
+//     return a / b
+// }
+
+// function calculate(action, a, b) {
+//     if ((a > 0 || a === 0) && (b > 0 || b === 0)) {
+//         switch (action) {
+//             case 'addition': {
+//                 return add(a, b);
+//             }
+//             case 'subtraction': {
+//                 return subtract(a, b);
+//             }
+//             case 'multiply': {
+//                 return multiply(a, b);
+//             }
+//             case 'divide': {
+//                 if (b === 0 || a === 0) {
+//                     return 'Please enter a value greater than 0'
+//                 }
+//                 return divide(a, b);
+//             }
+//             default: {
+//                 return 'Invalid Operation'
+//             }
+//         }
+//     } else {
+//         console.error('Please enter a valid number.')
+//     }
+
+// }
+
+
+
+
+// const result1 = calculate('divide', 3, 1)
+// console.log('result1', result1)
+
+// let a = '999';
+// let b = "22";
+
+
+// console.log('type a', typeof parseInt(a))
+// console.log("type b", typeof parseInt(b))
+
+// const result = parseInt(a) / parseInt(b)
+
+// console.log(parseFloat(result).toFixed(2))
+
+
+const listContainer = document.getElementById('list-container');
+
+// listContainer.innerHTML = `<ol>
+//     <li>apple</li>
+//     <li>banana</li>
+//     <li>orange</li>
+// </ol>`
+
+const array = ['apple', 'banana', 'orange', 'gwava', 'grapes', 'mango', 'kwiwi', 'pineapple']
+const orderedList = document.createElement('ol');
+
+for (let i = 0; i < array.length; i++) {
+    if (array[i] !== 'apple' && array[i] !== 'orange') {
+        const listItem = document.createElement('li')
+        listItem.innerText = array[i]
+        orderedList.appendChild(listItem)
+    }
+}
+
+listContainer.appendChild(orderedList)
+
